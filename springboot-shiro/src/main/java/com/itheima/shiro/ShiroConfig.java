49afc3c10b9c863068cf27e0571194bc570e7999
徐森 <xusen@cmiot.chinamobile.com>
2019-02-27 11:46:46 +0800

    1

---
+++ springboot-shiro/src/main/java/com/itheima/shiro/ShiroConfig.java
@@ -0,0 +1,118 @@
+package com.itheima.shiro;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
+
+/**
+ * Shiro的配置类
+ * @author lenovo
+ *
+ */
+@Configuration
+public class ShiroConfig {
+
+	/**
+	 * 创建ShiroFilterFactoryBean
+	 */
+	@Bean
+	public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager")DefaultWebSecurityManager securityManager){
+		ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+		
+		//设置安全管理器
+		shiroFilterFactoryBean.setSecurityManager(securityManager);
+		
+		//添加Shiro内置过滤器
+		/**
+		 * Shiro内置过滤器，可以实现权限相关的拦截器
+		 *    常用的过滤器：
+		 *       anon: 无需认证（登录）可以访问
+		 *       authc: 必须认证才可以访问
+		 *       user: 如果使用rememberMe的功能可以直接访问
+		 *       perms： 该资源必须得到资源权限才可以访问
+		 *       role: 该资源必须得到角色权限才可以访问
+		 */
+		Map<String,String> filterMap = new LinkedHashMap<String,String>();
+		/*filterMap.put("/add", "authc");
+		filterMap.put("/update", "authc");*/
+		
+		filterMap.put("/testThymeleaf", "anon");
+		//放行login.html页面
+		filterMap.put("/login", "anon");
+
+		//授权过滤器
+		//注意：当前授权拦截后，shiro会自动跳转到未授权页面
+		filterMap.put("/add", "perms[user:add]");
+		filterMap.put("/update", "perms[user:update]");
+		
+		filterMap.put("/*", "authc");
+		
+		//修改调整的登录页面
+		shiroFilterFactoryBean.setLoginUrl("/toLogin");
+		//设置未授权提示页面
+		shiroFilterFactoryBean.setUnauthorizedUrl("/noAuth");
+		
+		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
+		
+		
+		return shiroFilterFactoryBean;
+	}
+	
+	/**
+	 * 创建DefaultWebSecurityManager
+	 */
+	@Bean(name="securityManager")
+	public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm")UserRealm userRealm){
+		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+		//关联realm
+		securityManager.setRealm(userRealm);
+		return securityManager;
+	}
+	/**
+	 *  开启Shiro的注解(如@RequiresRoles,@RequiresPermissions),需借助SpringAOP扫描使用Shiro注解的类,并在必要时进行安全逻辑验证
+	 * 配置以下两个bean(DefaultAdvisorAutoProxyCreator和AuthorizationAttributeSourceAdvisor)即可实现此功能
+	 * @return
+	 */
+	@Bean
+	public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator(){
+		DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+		advisorAutoProxyCreator.setProxyTargetClass(true);
+		return advisorAutoProxyCreator;
+	}
+
+	/**
+	 * 开启aop注解支持
+	 * @param securityManager
+	 * @return
+	 */
+	@Bean
+	public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager")DefaultWebSecurityManager securityManager) {
+		AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+		authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+		return authorizationAttributeSourceAdvisor;
+	}
+	/**
+	 * 创建Realm
+	 */
+	@Bean(name="userRealm")
+	public UserRealm getRealm(){
+		return new UserRealm();
+	}
+	
+	/**
+	 * 配置ShiroDialect，用于thymeleaf和shiro标签配合使用
+	 */
+	/*@Bean
+	public ShiroDialect getShiroDialect(){
+		return new ShiroDialect();
+	}*/
+}
